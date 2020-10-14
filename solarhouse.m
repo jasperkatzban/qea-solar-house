d2f9a5e7d14d3e99993151c70ead5f6e0a8333e5
Jasper Katzban <jasperkatzban@gmail.com>
2020-10-14 16:14:00 -0400

    initial commit

---
+++ solarhouse.m
@@ -0,0 +1,136 @@
+% todo:
+% How does the storage unit thickness affect the house’s thermal behavior?
+% 
+% The thickness of the storage unit affects the temperature range
+% experienced by the floor and air in the house, but does not influence the
+% average temperature over time. The thinner the absorber, the more
+% fluctuation occurs on a daily basis, and the longer it would take for the
+% house to reach a comfortable average temperature over time.
+% 
+% How does the insulation thickness affect the house’s thermal behavior?
+% 
+% The thickness of the insulation controls the asymptotic behavior of the
+% system, and in turn the resultant temperature in the house, as well as the
+% time it takes to get there. The thinner the insulation, the less time it
+% takes to reach a steady state, and the lower the steady state temperature
+% will be. Reversing this, with thicker walls, there's more insulation and
+% therefore a higher resultant internal temperature, but it will take
+% longer to achieve this state.
+% 
+% Why?
+% 
+% Once again, we can compare the the absorber to a capacitor and the
+% insulation to a resistor, which means we've modeled something similar to
+% an RC circuit. In varying the thicknesses of each of these
+% parts, we're effectively changing the resultant air temperature
+% (analogous to voltage, affected by resistance) and the smoothing on the
+% air temperature (analogous to the cutoff frequency of an electronic
+% filter, which affects voltage over time). 
+
+clc, clear all; % clean slate
+
+% define params to vary or sweep
+absorber_thickness = .5; % m
+insulation_thickness = 1; % m
+
+
+num_days = 40
+tspan = [0 86400*num_days]; % s
+
+% house dimension parameters
+h_length = 6; % m
+h_width = 2; % m
+h_height = 3; % m
+
+g_height = 2; % m
+g_area = h_length * g_height; % m ^ 2
+% g_thick = 0.005; % m
+
+r_height = sqrt(h_width^2 + (h_height - g_height)^2); % m
+r_area = h_length * r_height; % m ^ 2
+% r_thick = 0.1; % m
+
+bwa_area = h_length * h_height; % m ^ 2
+swa_area = (g_height * h_width) + .5 * (h_width * (h_height - g_height)); % m ^ 2
+wa_thick = insulation_thickness; % m
+
+f_area = h_width * h_length; % m ^ 2
+f_thick = absorber_thickness; % m
+
+ins_net_area = bwa_area + 2 * swa_area + r_area + f_area;
+
+% house material properties
+f_density = 0; % kg / m ^ 2
+
+% material thermal properties
+h_glass = .7; % W/m^2-K
+h_in = 15; % W/m^2-K
+h_out = 30; % W/m^2-K
+
+k_wall = .4; % W/m-K
+
+v_air = swa_area * h_width; % m^3
+d_air = 1.225; %kg/m^3
+m_air = d_air * v_air; %kg
+c_air = 1012; %J/kg-K
+
+v_abs = f_area * f_thick; % m^3
+d_abs = 3000; % kg/m^3
+m_abs = d_abs * v_abs; % kg
+c_abs = 800; % J/kg-K
+
+C_abs = 800 * m_abs % J/K
+C_a = c_air * m_air; % J/K
+
+% partial resistances
+R_abs_air = 1/(h_in * f_area);
+
+R_air_wall = 1/(h_in * ins_net_area);
+R_wall_wall = wa_thick/(k_wall * ins_net_area);
+
+R_air_glass = 1/(h_in * g_area);
+R_glass_glass = 1/(h_glass * g_area);
+
+R_outer_air = 1/(h_out * ins_net_area);
+
+% equivalent resistances
+R_fa = R_abs_air; % total thermal resistance from floor to air
+R_ao = + 1/(1/(R_air_wall + R_wall_wall)+1/(R_air_glass + R_glass_glass)) + R_outer_air; % total thermal resistance from air to outside
+
+% f = @(t,T) [(1/C_abs)*(g_area*(-361*cos(pi*t/43200) + 224*cos(pi*t/21600) + 210) - ((T(1)-T(2))/R_fa)); ...
+%             (1/C_a)*((T(1)-T(2))/R_fa) - (T(2)-(6*sin((2*pi*t/86400)+3*pi/4)-3)/R_ao)];
+
+f = @(t,T) [(1/C_abs)*(g_area*(-361*cos(pi*t/43200) + 224*cos(pi*t/21600) + 210) - ((T(1)-T(2))/R_fa)); ...
+            (1/C_a)*(((T(1)-T(2))/R_fa) - (T(2) - -3)/R_ao)];
+
+[t,dT] = ode45(f, tspan, [0 0]); % compute the ode
+
+M = movmean(dT,3600*4); % 4 hour moving average
+
+% Plot temperature over time with optimized values
+f1 = figure(1);
+hold on;
+grid on;
+plot(t,dT,'-')
+plot(t,M, '-')
+h = legend("$T_{floor}$", "$T_{air}$", "$\overline{T_{floor}}$", "$\overline{T_{air}}$");
+set(h,'interpreter','Latex','FontSize',12)
+title("Temperatures in House & 4 hr Moving Average");
+xlabel("Time (seconds)");
+ylabel("Temperature (Celsius)");
+hold off
+
+% Plot temperature over time with optimized values over a single day
+f2 = figure(2);
+hold on;
+grid on;
+plot(t,dT,'-')
+plot(t,M, '-')
+h = legend("$T_{floor}$", "$T_{air}$", "$\overline{T_{floor}}$", "$\overline{T_{air}}$");
+set(h,'interpreter','Latex','FontSize',12);
+title("Temperatures in House & 4 hr Moving Average, Single Day");
+xlabel("Time (seconds)");
+ylabel("Temperature (Celsius)");
+x_start = 993000;
+axis([x_start x_start+86400 0 40])
+hold off
